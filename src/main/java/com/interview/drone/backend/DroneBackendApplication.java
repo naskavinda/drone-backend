ab1f707ddc700bda6688ef265ef3f9450b69e65e
naskavinda <naskavinda@gmail.com>
2023-02-01 22:51:04 +0530

    initial code structure

---
+++ src/main/java/com/interview/drone/backend/DroneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.interview.drone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/drone/backend/DroneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.drone.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
