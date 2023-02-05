bdf2470da68d4865875f8953a645f4ddee91e71e
naskavinda <naskavinda@gmail.com>
2023-02-05 23:30:54 +0530

    add test cases for the Register drone endpoint

---
+++ src/test/java/com/interview/drone/backend/controller/DroneControllerTest.java
@@ -0,0 +1,76 @@
+package com.interview.drone.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.interview.drone.backend.dto.DroneResponse;
+import com.interview.drone.backend.dto.RegisterDroneRequest;
+import com.interview.drone.backend.entity.DroneModel;
+import com.interview.drone.backend.entity.DroneState;
+import com.interview.drone.backend.service.DroneService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class DroneControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DroneService droneService;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void shouldReturnHttpStatusCreated_whenInputIsValid_forRegisterDrone() throws Exception {
+
+        DroneResponse response = new DroneResponse();
+        response.setSerialNumber("1234");
+        response.setWeightLimit(500);
+        response.setDroneState(DroneState.IDLE);
+        response.setDroneModel(DroneModel.LIGHT_WEIGHT);
+        response.setBatteryCapacity(100);
+
+        when(droneService.registerDrone(any(RegisterDroneRequest.class))).thenReturn(response);
+        MvcResult result = mockMvc
+                .perform(post("/api/drone")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"serialNumber\":\"1234\",\"droneModel\":\"LIGHT_WEIGHT\",\"weightLimitInGram\":500.00,\"batteryCapacity\":100}"))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        DroneResponse droneResponse = objectMapper.readValue(result.getResponse().getContentAsString(), DroneResponse.class);
+        assertThat(droneResponse).isEqualTo(response);
+        assertThat(result.getResponse().getStatus()).isEqualTo(201);
+    }
+
+    @Test
+    void shouldReturnHttpStatusBadRequest_whenInputIsInValid_forRegisterDrone() throws Exception {
+
+        MvcResult result = mockMvc
+                .perform(post("/api/drone")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"serialNumber\":\"12341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234\",\"droneModel\":\"LIGHT_WEIGHT\",\"weightLimitInGram\":-500.00,\"batteryCapacity\":122}"))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+        String actualResponse = result.getResponse().getContentAsString();
+        assertThat(actualResponse).isEqualTo("{\"weightLimitInGram\":\"Weight can not be less then 0\",\"serialNumber\":\"Serial Number max length is 100\",\"batteryCapacity\":\"Battery Capacity max value is 100\"}");
+        assertThat(result.getResponse().getStatus()).isEqualTo(400);
+    }
+
+
+}
